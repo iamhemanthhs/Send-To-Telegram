179a620c029804645485caa867e4c5d8f7c8fd7b
Hemanth H S <iam.hemanthhs@gmail.com>
2021-07-06 21:27:49 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Send To Telegram
+
+![Twitter Follow](https://img.shields.io/twitter/follow/theblizrdx?style=social)
+
+## About
+A windows application used to send the important messages to Telegram account using Telegram bot. 
+
+## Screenshots:
+![ScreenShot](/Screenshots/1.png?raw=true "Screenshot-1")
+
+## Author
+
+Hemanth H S(https://github.com/iamhemanthhs)
+
+## Tools Used:
+- Python 3
+- Tkinter for GUI
+- pycharm IDE
+- Telegram Bot
+
+---
---
+++ app.py
@@ -0,0 +1,40 @@
+import requests
+from tkinter import *
+from tkinter import Toplevel,messagebox,filedialog
+from tkinter.ttk import Treeview
+from tkinter import ttk
+import tkinter as tk
+
+root = Tk()
+root.title('Save To Telegram')
+root.config(bg='#0e1621')
+root.geometry('600x70+600+400')
+root.iconbitmap('icon.ico')
+root.resizable(False,False)
+
+mesgval=StringVar()
+
+def sendmessage():
+    message = mesgval.get()
+    token = '1438386037:AAE9YnLpyQ5MpNso4HsC684Q_utEjeFIP1s' #Telegram Bot Token
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    data = {'chat_id': {627491741}, 'text': {message}} #Chat Id of the User
+    requests.post(url, data).json()
+
+Frame1=Frame(root,bg='#243142')
+Frame1.place(x=520,y=15,width=70,height=40)
+
+sendbtn=Button(Frame1,text='Send',font=('Italic',10,NORMAL),bd=6,bg='#17212b',fg='white',command=sendmessage)
+sendbtn.place(x=0,y=0,width=70,height=40)
+
+mesgval=StringVar()
+
+mesgbx = Entry(root, font=('Italic', 13, NORMAL),bg='#17212b',fg='white',insertbackground='white',bd=1, textvariable=mesgval)
+mesgbx.place(x=10, y=14,width=500,height=40)
+
+root.mainloop()
+
+
+
+
+
