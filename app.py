355c444340677970cf01fbc6808a9d8bc7aa3643
Hemanth H S <iam.hemanthhs@gmail.com>
2022-01-18 14:20:52 +0530

    Testx

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from tkinter import *
-from tkinter import Toplevel, messagebox, filedialog
+from tkinter import Toplevel,messagebox,filedialog
 from tkinter.ttk import Treeview
 from tkinter import ttk
 import tkinter as tk
@@ -10,32 +10,33 @@ root.title('Save To TelegramX')
 root.config(bg='#0e1621')
 root.geometry('600x70+600+400')
 root.iconbitmap('icon.ico')
-root.resizable(False, False)
+root.resizable(False,False)
 
-mesgval = StringVar()
-
+mesgval=StringVar()
 
 def sendmessage():
     message = mesgval.get()
-    token = '5082767515:AAE8ql_flAf4P13XHuZKoI0DnLas6CI6LqM'  # Telegram Bot Token
+    token = '5022255490:AAFp1MMWKXq84__4VVloMurORNWDZhvwpcs' #Telegram Bot Token
     url = f'https://api.telegram.org/bot{token}/sendMessage'
-    data = {'chat_id': {5060166011}, 'text': {message}}  # Chat Id of the User
+    data = {'chat_id': {5060166011}, 'text': {message}} #Chat Id of the User
     requests.post(url, data).json()
 
-# Kumar swami -- > 874434712
+# Kumar swami -- > 874434712 
 
+Frame1=Frame(root,bg='#243142')
+Frame1.place(x=520,y=15,width=70,height=40)
 
-Frame1 = Frame(root, bg='#243142')
-Frame1.place(x=520, y=15, width=70, height=40)
+sendbtn=Button(Frame1,text='Send',font=('Italic',10,NORMAL),bd=6,bg='#17212b',fg='white',command=sendmessage)
+sendbtn.place(x=0,y=0,width=70,height=40)
 
-sendbtn = Button(Frame1, text='Send', font=('Italic', 10, NORMAL),
-                 bd=6, bg='#17212b', fg='white', command=sendmessage)
-sendbtn.place(x=0, y=0, width=70, height=40)
+mesgval=StringVar()
 
-mesgval = StringVar()
-
-mesgbx = Entry(root, font=('Italic', 13, NORMAL), bg='#17212b',
-               fg='white', insertbackground='white', bd=1, textvariable=mesgval)
-mesgbx.place(x=10, y=14, width=500, height=40)
+mesgbx = Entry(root, font=('Italic', 13, NORMAL),bg='#17212b',fg='white',insertbackground='white',bd=1, textvariable=mesgval)
+mesgbx.place(x=10, y=14,width=500,height=40)
 
 root.mainloop()
+
+
+
+
+
